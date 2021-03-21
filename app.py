3b6157bcfc631b6c709fee197f3330277bf91ee6
Ramesh Suragam <ramesh.suragam@gmail.com>
2021-03-21 18:26:26 +0000

    draft 2 - pickle implementation

--- app.py
+++ app.py
@@ -14,13 +14,13 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [int(x) for x in request.form.values()]
+    int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
-    output = round(prediction[0], 2)
+    output = round(prediction[0], 8)
 
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+    return render_template('index.html', prediction_text='Sales Rate should be {}'.format(output))
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
